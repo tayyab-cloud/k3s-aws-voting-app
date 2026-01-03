87e3c71704ab736535bef67e20fbcbc9a2763f7c
tayyab-cloud <https://github.com/tayyab-cloud>
2026-01-04 00:25:59 +0500

    Fix: Added default values to prevent pipeline hang

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,37 @@
 variable "region" {
   description = "AWS Region to deploy resources"
   type        = string
+
 }
 
 variable "project_name" {
   description = "Project name prefix for tagging resources"
   type        = string
+  default = "k3s-voting-app"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_1" {
   description = "CIDR block for Public Subnet 1 (Master Node)"
   type        = string
+  default = "10.0.1.0/24"
 }
 
 variable "public_subnet_2" {
   description = "CIDR block for Public Subnet 2 (Future Use/HA)"
   type        = string
+  default = "10.0.2.0/24"
 }
 
 variable "instance_type" {
   description = "EC2 Instance Type (Free Tier: t2.micro or t3.micro)"
   type        = string
+  default = "t2.micro"
 }
 
 variable "public_key_path" {
