a9cb98bf8e03bb6fa591b53e69f2e857b91d4aef
tayyab-cloud <https://github.com/tayyab-cloud>
2026-01-25 17:39:12 +0500

    Force: Hardcoded key with new name to bypass AWS cache

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@
 
 # 1. Create Key Pair Globally (Once)
 resource "aws_key_pair" "k3s_auth" {
-  key_name   = "${var.project_name}-key"
-  public_key = file(var.public_key_path)
+  key_name   = "k3s-key-hardcoded-v1"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvgFKk62TDZpe5eiPfN2oAxb7+sfuzY8ZOrhPPHh1jmaAC3DF7kdgm34WBu5i8nTBTgArpTRGJEA13p4oPqQXXlmMqQ0ohYttc/eD13ujbOgBcMvijAm5k/U2ehJ5n8j1CnQg5xtYLz5+xG2AQ0fhnfF8MTFvaaBcYB9QAmpEcLrDj4O1tRtmuhbwzn7B1nJOE96Xy8uNkV8KB2lf3QRrzHFJiusOrPJqKsYaNQc7I9TFYIK6O5RYg1VhVGT7kKhIhHE5NLAJFgAgXnKzJun6DvjTy44mw17Lkbyt4GOS4CwuqlQAUkUpNx1km+NH27OkX+DG/SjsCdOPQKbuB+LIdQhcn+nz6LZzWfe74Hgy0kET0zOSgP5TlPEhShXj3yrQalVfVXgolaKGm3dHjXcovNTuIyP01LPhvtwFd55iCrxS69NarQMTlzt14B468G8ZdiX4yVJvaPQ9kkABWSga4VCeJDf99Z5fFo9/EUSQgpDk3Q9RbLl/9jgPaBqHT28WGy7W50Xl2g/E5sFJLesaS6W10szcvKAOhFBRlSyC8MfvwPPV7xfbdqq0Mp4rYvc2XcfHRum0213L8xUyvkZ3in2B8Ssgv9+9EqPpccG8QMyGKapc6KowgK6ZOsxSTLfOuOhQJVDMzHehBHAMEP3KcqcMTROue81dE26eyioHbqQ== k3s-user"
 }
 
 # 2. Call VPC Module
